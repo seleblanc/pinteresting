3989b1a0f491d1d5a37b3947d165d8be6aecaa7c
Serge Emile LeBlanc <sergeleblanc@seleblanc.com>
2014-12-06 10:01:10 -0800

    Added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   #get 'pages/home'
   root 'pages#home'
   get "about" => "pages#about" # Creates about_path
+  get "contact" => "pages#contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
